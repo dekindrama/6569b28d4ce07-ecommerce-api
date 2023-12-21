3eaf483b16b2284f30c8632af2d9eec4bf405e87
indra <92654577+dekindrama@users.noreply.github.com>
2023-12-21 15:06:44 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -27,9 +27,9 @@ Route::middleware(['auth:sanctum'])->group(function () {
 });
 
 //* items
+Route::get('items', [ItemController::class, 'getListItems'])->name('items.index');
+Route::get('items/{item_id}', [ItemController::class, 'getDetailItem'])->name('items.show');
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('items', [ItemController::class, 'getListItems'])->name('items.index');
-    Route::get('items/{item_id}', [ItemController::class, 'getDetailItem'])->name('items.show');
     Route::post('items', [ItemController::class, 'storeItem'])->name('items.store');
     Route::post('items/{item_id}', [ItemController::class, 'updateItem'])->name('items.update');
     Route::post('items/{item_id}/delete', [ItemController::class, 'softDeleteItem'])->name('items.delete');
